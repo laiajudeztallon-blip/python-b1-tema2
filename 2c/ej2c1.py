4b7949efcb65e6467c309c11ce2d2f25199cb272
laiajudeztallon-blip <laiajudeztallon@gmail.com>
2025-12-03 10:52:58 +0100

    Update ej2c1.py

--- 2c/ej2c1.py
+++ 2c/ej2c1.py
@@ -51,7 +51,13 @@ Exemple:
 
 def convert_to_integer(string):
     # Write here your code
-    pass
+    def convert_to_integer(string):
+    try:
+        return int(string)
+    except ValueError:
+        return "The string cannot be converted to an integer"
+    except Exception as error:
+        return f"An unexpected error has occurred: {error}"
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
