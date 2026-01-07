d2c6dc9bf04b931cc4a7166ba254e27a9ee46d99
laiajudeztallon-blip <laiajudeztallon@gmail.com>
2026-01-07 13:08:57 +0100

    Update ej2a3.py

--- 2a/ej2a3.py
+++ 2a/ej2a3.py
@@ -91,17 +91,31 @@ Exemple:
 
 def add_student_by_value(list_students, new_student):
     # Write here your code
-    pass
+    list_new_students = list_students.copy()
+    list_new_students.append(new_student)
+    return list_new_students
 
 
 def add_student_by_reference(list_students, new_student):
     # Write here your code
-    pass
+    list_students.append(new_student)
+    return list_students
 
 
 def main(list_students, new_student_by_value, new_student_by_reference):
     # Write here your code
-    pass
+    print(f"Original student list {list_students}")
+    print(
+        f"Student list by value",
+        add_student_by_value(list_students, new_student_by_value),
+    )
+    print(
+        f"Student list by reference",
+        add_student_by_reference(list_students, new_student_by_reference),
+    )
+    print(f"Original student list {list_students}")
+    return list_students
+
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
